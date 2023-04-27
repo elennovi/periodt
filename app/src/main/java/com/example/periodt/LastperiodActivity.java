1add8a522f499454dcff72f2c6aef1a618e187f7
erik <erikzubi@ucm.es>
2023-04-27 18:31:43 +0200

    now preferences are applied, notifications dont show

--- app/src/main/java/com/example/periodt/LastperiodActivity.java
+++ app/src/main/java/com/example/periodt/LastperiodActivity.java
@@ -1,7 +1,9 @@
 package com.example.periodt;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -12,6 +14,9 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class LastperiodActivity extends AppCompatActivity {
 
@@ -44,7 +49,6 @@ public class LastperiodActivity extends AppCompatActivity {
         continue_btn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 submitData();
-                redirectCalendar();
             }
         });
 
@@ -57,27 +61,45 @@ public class LastperiodActivity extends AppCompatActivity {
         startActivity(intent);
     }
     private void submitData(){
-        // Duration
-        Spinner duration_sp = (Spinner) findViewById(R.id.period_duration);
-        String duration = duration_sp.getSelectedItem().toString();
-
         // Last one
         EditText last_et = (EditText) findViewById(R.id.select_last_date);
         String last = last_et.getText().toString();
+        Date today = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        String today_str = sdf.format(today);
+        // if no date has been introduced or the date is yet to come
+        if(last.equals("") || last == null || last.compareTo(today_str) > 0){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    dialog.cancel();
+                }
+            });
+            builder.setMessage(R.string.invalid_date)
+                    .setTitle(R.string.error);
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
+        else{
+            // Duration
+            Spinner duration_sp = (Spinner) findViewById(R.id.period_duration);
+            String duration = duration_sp.getSelectedItem().toString();
 
-        // Regularity
-        Spinner regularity_sp = (Spinner) findViewById(R.id.regular_period);
-        String cycle = regularity_sp.getSelectedItem().toString();
+            // Regularity
+            Spinner regularity_sp = (Spinner) findViewById(R.id.regular_period);
+            String cycle = regularity_sp.getSelectedItem().toString();
 
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        String uid = prefs.getString("uid", "0");
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+            String uid = prefs.getString("uid", "0");
 
-        // Submit the data
-        Log.i("DURATION", duration);
-        Log.i("LAST_PERIOD", last);
-        Log.i("CYCLE", cycle);
-        Log.i("UID", uid);
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putBoolean("periodNotifSent", false);
+            editor.putBoolean("fertileNotifSent", false);
+            editor.apply();
 
-        db.registerPeriod(last, duration, cycle, uid);
+            db.registerPeriod(last, duration, cycle, uid);
+
+            redirectCalendar();
+        }
     }
 }
